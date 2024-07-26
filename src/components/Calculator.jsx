ff14c686ab94ebaad981c4f00fc2e30ee236813e
Otavio Cuellar <cuellarotavio0@gmail.com>
2024-07-26 00:15:13 -0300

    beta 1.1

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Calculator.css'
+import CalculatorButtons from './CalculatorButtons';
 
 const Calculator = () => {
   return (
     <div>
-        
+        <CalculatorButtons />
     </div>
   )
 }
